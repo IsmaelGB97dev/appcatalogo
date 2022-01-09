299f27fb8bdd8d2f230537bb48e23360a7f7cd64
Ismael Gutierrez <ismaelgutierrezdev@gmail.com>
2022-01-08 21:42:42 -0600

    Entender manejo de rutas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import { Home } from './componentes/Home';
+import { ContainerProductos } from './componentes/ContainerProductos';
+ 
 function App() {
   return (
-    <p>Proximamente...</p>
+    <BrowserRouter>
+      <ContainerProductos />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/componentes/ContainerProductos.jsx
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+import { Link } from 'react-router-dom'; 
+
+export function ContainerProductos() {
+    return (
+        <div>
+            <Link to="/home?pr=1">Home</Link>
+        </div>
+    );
+}
